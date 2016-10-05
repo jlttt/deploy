9190ef3b3f6ce0f61c022d57f718c9f79fd47734
jean-luc <jean-luc@pmpconcept.com>
2016-10-05 22:54:10 +0200

    add ci file configuration

--- src/tests/units/Deployer.php
+++ src/tests/units/Deployer.php
@@ -1,9 +1,29 @@
 <?php
-namespace Pmp\Deploy\tests\units\Deployer;
+namespace Pmp\Deploy\tests\units;
 
 use atoum;
 
 class Deployer extends atoum
 {
+    public function testGetCreatedFiles()
+    {
+        $mockDeployer = new \mock\Deployer();
+        $mockDeployer->getMockController()->getSourceFiles = function() {
+            return [];
+        };
+        $mockDeployer->getMockController()->getDestinationFiles = function() {
+            return [];
+        }; 
 
+    }
+
+    public function testGetUpdatedFiles()
+    {
+
+    }
+
+    public function testGetDeletedFiles()
+    {
+
+    }
 }
